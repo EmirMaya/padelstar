c229972fa19366acf36b893c2bb968f148f87707
emirMaya <emirmayaok@gmail.com>
2022-03-28 21:00:04 -0300

    navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return(
+        <nav class = "navBar">
+            <button class = "buttons">
+                INICIO
+            </button>
+
+            <button class = "buttons">
+                PALETAS
+            </button>
+
+            <button class = "buttons">
+                INDUMENTARIA
+            </button>
+
+            <button class = "buttons">
+                ACCESORIOS
+            </button>
+        </nav>
+    )
+}
+
+export default NavBar;
